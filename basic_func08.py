e36c6ac93e55bced69b47b522ff27b349078d5e7
user1 <backenduz@gmail.com>
2022-11-14 17:39:11 +0500

    sakkiz

--- basic_func08.py
+++ basic_func08.py
@@ -1,5 +1,7 @@
 # Create a function called main.
-
+def main(a):
 # Create function arguments a.
-
 # Increase the value of a to one and return.
+     value = a + 1
+     return value
+print(main(2))
